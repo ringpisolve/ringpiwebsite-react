debf258a24c668e91d7ffa23470c79f60c249747
ringpisolve@gmail.com <ringpisolve2019>
2020-05-13 22:31:54 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <h1>Ring Pi!</h1>,
+        <h5>-Build your Ideas</h5>
+    </div>
+  );
+}
+
+export default App;
