c8d1fe2e345258a40a5bd94b9e76bf5ee2eb3158
ringpisolve@gmail.com <ringpisolve2019>
2020-05-14 07:35:11 +0530

    Updates

---
+++ precache-manifest.dcf350bc520e835318acc67c95c69998.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6123815f37d97c4492b4bd14150b75",
+    "url": "/ringpiwebsite-react/index.html"
+  },
+  {
+    "revision": "b65b95a3b24932181ed3",
+    "url": "/ringpiwebsite-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "323c88edebb3b5ef61da",
+    "url": "/ringpiwebsite-react/static/js/2.9ce0847a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ringpiwebsite-react/static/js/2.9ce0847a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65b95a3b24932181ed3",
+    "url": "/ringpiwebsite-react/static/js/main.b18bb6b6.chunk.js"
+  },
+  {
+    "revision": "5d18007228fedd7aac47",
+    "url": "/ringpiwebsite-react/static/js/runtime-main.ef73f3d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ringpiwebsite-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
